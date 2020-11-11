63ab9d1f6d9e7991592929baf554e35fea6e540c
Peter Hanley <hanley.peter@gmail.com>
2020-11-11 13:41:13 +0800

    Tested component

--- src/components/WeatherTitle/index.tsx
+++ src/components/WeatherTitle/index.tsx
@@ -14,3 +14,5 @@ export const WeatherTitle: FC<IWeatherTitle> = ({ value }) => {
     </div>
   )
 }
+
+// <WeatherTitle value="Shower" />
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export { Temperature } from './Temperature'
 ||||||| parent of 0cb9210... First commit
 =======
 export { IconButton } from './IconButton'
 >>>>>>> 0cb9210... First commit
+||||||| parent of d7c0ea4... Tested component
+export { IconButton } from './IconButton'
+=======
+export { WeatherTitle } from './WeatherTitle/index'
+>>>>>>> d7c0ea4... Tested component
