f8b4e985b10c986b546cf1cc4584459a6858fd36
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-13 20:25:44 +0800

    Add mapping of weather icons

---
+++ src/components/WeatherIcon/WeatherIcons.tsx
@@ -0,0 +1,23 @@
+import Clear from '../../assets/Clear.png'
+import Hail from '../../assets/Hail.png'
+import HeavyCloud from '../../assets/HeavyCloud.png'
+import HeavyRain from '../../assets/HeavyRain.png'
+import LightCloud from '../../assets/LightCloud.png'
+import LightRain from '../../assets/LightRain.png'
+import Shower from '../../assets/Shower.png'
+import Sleet from '../../assets/Sleet.png'
+import Snow from '../../assets/Snow.png'
+import Thunderstorm from '../../assets/Thunderstorm.png'
+
+export const IMAGES = {
+  clear: Clear,
+  hail: Hail,
+  heavyCloud: HeavyCloud,
+  heavyRain: HeavyRain,
+  lightCloud: LightCloud,
+  lightRain: LightRain,
+  shower: Shower,
+  sleet: Sleet,
+  snow: Snow,
+  thunderstorm: Thunderstorm,
+}
