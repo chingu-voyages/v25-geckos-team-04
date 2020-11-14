8ffdae0799f05e4fa3aa3fd367e0f507562e237c
hananina <bubblesyo86@gmail.com>
2020-11-14 16:39:53 +0900

    add style

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ export const App: FC = () => {
   return (
     <div>
       <h1>Weather App</h1>
-      <Temperature value={15} scale="celsius" />
-      <Button value={15} scale="celsius" />
     </div>
   )
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react'
 import styles from './Button.module.css'
 
 interface IButton {
-  color?: 'primary' | 'secondary'
+  style: 'primary' | 'accent'
   value: string | number
 }
 
-export const Button: FC<IButton> = ({ color = 'primary', value }) => {
+export const Button: FC<IButton> = ({ style = 'primary', value }) => {
   return (
     <>
-      <button className={color === 'primary' ? 'styles.button-primary' : 'styles.button-secondary'}>
+      <button
+        type='button'
+        className={style === 'primary' ? styles.btnPrimary : styles.btnAccent}
+      >
         {value}
       </button>
     </>
