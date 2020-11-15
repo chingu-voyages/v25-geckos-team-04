68e0e05ac31fdd0dae72548eacb3472ec5879bdd
hananina <bubblesyo86@gmail.com>
2020-11-16 00:10:38 +0900

    add onclick function

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,17 @@
-import React, { Children, FC } from 'react'
+import React, { Children, FC, MouseEvent } from 'react'
 import styles from './Button.module.css'
 
 interface IButton {
   style: 'primary' | 'accent'
+  // TODO need ti add type for onClick. what I write here is throwing an error...
+  onClick: (ev: React.MouseEvent<HTMLElement, MouseEvent>) => void
 }
 
-export const Button: FC<IButton> = ({ style = 'primary', children }) => {
+export const Button: FC<IButton> = ({ style = 'primary', onClick, children }) => {
   return (
     <button
       type='button'
+      onClick={onClick}
       className={style === 'primary' ? styles.btnPrimary : styles.btnAccent}
     >
       {children}
