4bec9a7062e239e0232562791dd721e95ba2373c
Hana Tokumoto <bubblesyo86@gmail.com>
2020-11-16 22:42:35 +0900

    #12 Add current place (#22)
    * add current place
    
    * update style
    
    * update style
    
    * add empty line

---
+++ src/components/CurrentPlace/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import PlaceIcon from '@material-ui/icons/Place';
+import styles from './CurrentPlace.module.css'
+
+interface ICurrentPlace {
+  place: string
+}
+
+export const CurrentPlace: FC<ICurrentPlace> = ({ place }) => {
+  return (
+    <div className={styles.place}>
+      <PlaceIcon className={styles.symbol} />
+      <span className={styles.title}>{place}</span>
+    </div>
+  )
+}
