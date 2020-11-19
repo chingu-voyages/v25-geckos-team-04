bb71bffde08b1734d3431b0db623ed2360fdf7ec
Hana Tokumoto <bubblesyo86@gmail.com>
2020-11-19 13:36:03 +0900

    #7 Add button component (#20)
    * add files and base
    
    * update button and type
    
    * add style
    
    * fixmargin
    
    * add inner text as children
    
    * add onclick function
    
    * Fix onClick type
    
    * Add pointer cursor and change style prop to optional color property
    
    * Format code
    
    Co-authored-by: Stanley Thijssen <stanley.thijssen@gmail.com>

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC, MouseEvent } from 'react'
+import styles from './Button.module.css'
+
+interface IButton {
+  color?: 'primary' | 'accent'
+  onClick: (e: MouseEvent<HTMLElement>) => void
+  children: React.ReactNode
+}
+
+export const Button: FC<IButton> = ({
+  color = 'primary',
+  onClick,
+  children,
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${styles.container} ${
+        color === 'primary' ? styles.primary : styles.accent
+      }`}
+    >
+      {children}
+    </button>
+  )
+}
--- src/components/WeatherTitle/index.tsx
+++ src/components/WeatherTitle/index.tsx
@@ -1,18 +1,12 @@
 import React, { FC } from 'react'
 import styles from './WeatherTitle.module.css'
 
-
 interface IWeatherTitle {
   value: string
 }
 
 export const WeatherTitle: FC<IWeatherTitle> = ({ value }) => {
-
-  return (
-    <div className={styles.weatherTitle}>
-      {value}
-    </div>
-  )
+  return <div className={styles.weatherTitle}>{value}</div>
 }
 
 // <WeatherTitle value="Shower" />
