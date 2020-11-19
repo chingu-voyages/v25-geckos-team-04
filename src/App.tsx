9251a780757a880f8bc3f9ce66849a07c9f8cf01
Peter Hanley <hanley.peter@gmail.com>
2020-11-19 11:25:17 +0800

    Updated component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
+import { WindDirection } from './components'
 
 export const App: FC = () => {
   return (
     <div>
       <h1>Weather App</h1>
+      <WindDirection degrees={180} />
     </div>
   )
 }
