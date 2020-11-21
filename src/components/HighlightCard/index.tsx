f7766f86e044e8c1234ff98f1f4808ac6c19ee52
Peter Hanley <hanley.peter@gmail.com>
2020-11-21 19:25:26 +0800

    Split inner content of Highlight Card into four sub-components

---
+++ src/components/HighlightCard/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import styles from './HighlightCard.module.css'
+
+interface IHighlightCard {
+  children: React.ReactNode
+}
+
+export const HighlightCard: FC<IHighlightCard> = ({ children }) => {
+  return <div className={styles.highlightcard}>{children}</div>
+}
---
+++ src/components/Visibility/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import styles from './Visibility.module.css'
+
+interface IVisibility {
+  metric: 'celsius' | 'fahrenheit'
+  value: number
+}
+
+export const Visibility: FC<IVisibility> = ({ metric = 'celcius', value }) => {
+  return (
+    <>
+      <div className={styles.title}>Visibility</div>
+      <div className={styles.main}>
+        <span className={styles.value}>{value}</span>
+        {metric === 'celsius' ? 'km' : 'miles'}
+      </div>
+    </>
+  )
+}
