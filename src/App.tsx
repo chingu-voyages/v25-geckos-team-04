731a0d8876c9904a4835b734a3a918bce069a6aa
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-21 13:34:23 +0800

    Remove wind direction from page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react'
-import { WindDirection } from './components'
 
 export const App: FC = () => {
   return (
     <div>
       <h1>Weather App</h1>
-      <WindDirection degrees={180} />
     </div>
   )
 }
