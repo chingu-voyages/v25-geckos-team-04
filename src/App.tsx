5c9a21f9f62b99dc0ad56f8c51c1e33445c57192
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-22 17:01:54 +0800

    Add Sidebar dummy component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react'
+import { Sidebar } from './components/Sidebar'
 
 export const App: FC = () => {
   return (
-    <div>
+    <div style={{ display: 'flex', height: '100%' }}>
+      <Sidebar />
       <h1>Weather App</h1>
     </div>
   )
