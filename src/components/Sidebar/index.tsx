64ead5b69971c8a37f60526de85024e5609441f7
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-22 17:01:54 +0800

    Add Sidebar dummy component

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import styles from './Sidebar.module.css'
+import { Button } from '../Button'
+import { IconButton } from '../IconButton'
+import { WeatherIcon } from '../WeatherIcon'
+import { WeatherTitle } from '../WeatherTitle'
+import { Temperature } from '../Temperature'
+import { DateComponent } from '../DateComponent'
+import { CurrentPlace } from '../CurrentPlace'
+
+export const Sidebar: FC = () => {
+  return (
+    <aside className={styles.container}>
+      <div className={styles.navigationHeader}>
+        <Button onClick={() => console.log('Hello')}>Search for places</Button>
+        <IconButton name="gps" />
+      </div>
+      <div className={styles.weatherIconContainer}>
+        <div className={styles.weatherIcon}>
+          <WeatherIcon icon="shower" size="full" />
+        </div>
+      </div>
+      <Temperature value={15} />
+      <WeatherTitle value="Shower" />
+      Today • <DateComponent date={new Date()} formatString="eee, dd MMM" />
+      <CurrentPlace place="Hong Kong" />
+    </aside>
+  )
+}
