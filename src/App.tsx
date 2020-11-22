cbec6a9b7be8e37eca99fac4fd10f69ccbc82931
hananina <bubblesyo86@gmail.com>
2020-11-22 12:06:07 +0900

    remove demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React, { FC } from 'react'
-import { WeatherCard } from './components'
 
 export const App: FC = () => {
-
   return (
     <div>
       <h1>Weather App</h1>
-      <WeatherCard
-        date={new Date()}
-        formatString='eee d MMM'
-        icon='heavyCloud'
-        iconSize='small'
-        temperatureHighest={15}
-        temperatureLowest={15}
-        temperatureSize='small'
-        metric="celsius"
-      />
     </div>
   )
 }
