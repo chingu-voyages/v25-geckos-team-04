ebaa7aade8b1be6d5ca800176d10467abc0e6405
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-30 10:47:21 +0800

    Wrap all highlight components with the HighlightCard

--- src/components/Visibility/index.tsx
+++ src/components/Visibility/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { HighlightCard } from '../HighlightCard'
 import styles from './Visibility.module.css'
 
 interface IVisibility {
@@ -8,12 +9,12 @@ interface IVisibility {
 
 export const Visibility: FC<IVisibility> = ({ metric = 'celsius', value }) => {
   return (
-    <>
+    <HighlightCard>
       <div className={styles.title}>Visibility</div>
       <div className={styles.main}>
         <span className={styles.value}>{value}</span>
         {metric === 'celsius' ? 'km' : 'miles'}
       </div>
-    </>
+    </HighlightCard>
   )
 }
