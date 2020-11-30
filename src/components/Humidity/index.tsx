976350a6cf571423f7c005f3cce7d8e7d56c75c2
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-11-30 10:47:21 +0800

    Wrap all highlight components with the HighlightCard

--- src/components/Humidity/index.tsx
+++ src/components/Humidity/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { HighlightCard } from '../HighlightCard'
 import styles from './Humidity.module.css'
 
 interface IHumidity {
@@ -7,11 +8,11 @@ interface IHumidity {
 
 export const Humidity: FC<IHumidity> = ({ value }) => {
   return (
-    <>
+    <HighlightCard>
       <div className={styles.title}>Humidity</div>
       <div className={styles.main}>
         <span className={styles.value}>{value}</span>%
       </div>
-    </>
+    </HighlightCard>
   )
 }
