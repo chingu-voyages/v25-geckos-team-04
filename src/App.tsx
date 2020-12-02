9483063708080d910b2395721eee5a8f763d53b0
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-12-02 23:07:10 +0800

    Add responsive sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { FC } from 'react'
 import { Sidebar } from './components/Sidebar'
 
 export const App: FC = () => {
+  // Will have appContainer class for now until page component is build to support responsive sidebar;
   return (
-    <div style={{ display: 'flex', height: '100%' }}>
+    <div className="appContainer">
       <Sidebar />
       <h1>Weather App</h1>
     </div>
