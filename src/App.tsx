3d3c385219be54b2b3718335334e1ddfe27a2810
Peter Hanley <hanley.peter@gmail.com>
2020-12-07 15:01:38 +0800

    Renamed component, added functions to get weather data from location and search string

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import React, { FC } from 'react'
 import { Sidebar } from './components/Sidebar'
 import { AppProvider } from './contexts/AppContext'
 import { MainPage } from './components/MainPage'
+import CurrentWeatherByCityContainer from './containers/CurrentWeatherByCityContainer'
 
 export const App: FC = () => {
   // Will have appContainer class for now until page component is build to support responsive sidebar;
   return (
     <div className="appContainer">
       <AppProvider>
-        <Sidebar />
-        <MainPage />
+        <CurrentWeatherByCityContainer>
+          <Sidebar />
+          <MainPage />
+        </CurrentWeatherByCityContainer>
       </AppProvider>
     </div>
   )
