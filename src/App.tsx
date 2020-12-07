b4fd6e86b1851c77d4bbc1b9f31ec22eafc4d81b
Peter Hanley <hanley.peter@gmail.com>
2020-12-07 11:28:24 +0800

    Adjusted import

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { FC } from 'react'
 import { Sidebar } from './components/Sidebar'
-import { MetricProvider } from './contexts/MetricContext'
+import { AppProvider } from './contexts/AppContext'
 
 export const App: FC = () => {
   // Will have appContainer class for now until page component is build to support responsive sidebar;
   return (
     <div className="appContainer">
-      <MetricProvider>
+      <AppProvider>
         <Sidebar />
         <h1>Weather App</h1>
-      </MetricProvider>
+      </AppProvider>
     </div>
   )
 }
