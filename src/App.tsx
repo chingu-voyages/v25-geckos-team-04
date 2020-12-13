2fdbe1de47f459730d66dd5fc5db9066ae898f41
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-12-13 11:30:30 +0800

    Add axios plus axios default configuration and updated the currentDailyWeatherContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 import { AppProvider } from './contexts/AppContext'
 import { MainPage } from './components/MainPage'
-import CurrentWeatherByCityContainer from './containers/CurrentWeatherByCityContainer'
+import { CurrentDailyWeatherContainer } from './containers'
 
 export const App: FC = () => {
   // Will have appContainer class for now until page component is build to support responsive sidebar;
   return (
     <div className="appContainer">
       <AppProvider>
-        <CurrentWeatherByCityContainer />
+        <CurrentDailyWeatherContainer />
         <MainPage />
       </AppProvider>
     </div>
